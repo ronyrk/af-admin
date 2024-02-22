bc1b9e61adb0d1d38607ba05fdee262234526d2b
rakibulhasan933 <rakibulmd933@gmail.com>
2024-02-23 01:39:52 +0600

    [ADD] branch

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "@/components/ui/table";
+import { Button } from "@/components/ui/button"
+import Link from 'next/link';
+import { BranchIProps } from '@/types';
+import { unstable_noStore } from 'next/cache';
+import { Input } from '@/components/ui/input';
+import DeleteButton from '@/components/DeleteButton';
+import { Loader2Icon } from 'lucide-react';
 
-function Dashboard() {
+async function BranchList() {
+	unstable_noStore();
+	let res = await fetch('https://af-admin.vercel.app/api/branch');
+	if (!res.ok) {
+		throw new Error("Failed to fetch data");
+	};
+	let branches: BranchIProps[] = await res.json();
+
 	return (
-		<div>Dashboard</div>
+		<TableBody>
+			{
+				branches.map((item, index: number) => (
+					<TableRow key={index}>
+						<TableCell className="font-medium">{item.code}</TableCell>
+						<TableCell className="font-medium uppercase">{item.branchName}</TableCell>
+						<TableCell className="font-medium uppercase" >{item.district}</TableCell>
+						<TableCell className="font-medium uppercase">{item.ps}</TableCell>
+						<TableCell className="font-medium uppercase">
+							<Suspense fallback={<h2>Loading...</h2>}>
+								<DeleteButton username={item.username} />
+							</Suspense>
+						</TableCell>
+					</TableRow>
+				))
+			}
+		</TableBody>
+	)
+};
+
+
+
+async function page() {
+	return (
+		<div className='flex flex-col'>
+			<div className="p-2 flex justify-between ">
+				<Button asChild>
+					<Link className=' bg-color-main hover:bg-color-sub' href={`dashboard/branch_create`}>Branch Create</Link>
+				</Button>
+				<Input className='w-64' type="text" placeholder="Search" />
+			</div>
+			<Table>
+				<TableHeader>
+					<TableRow>
+						<TableHead>CODE</TableHead>
+						<TableHead className='md:w-[300px]'>BRANCH</TableHead>
+						<TableHead>DISTRICT</TableHead>
+						<TableHead>PS(TANA)</TableHead>
+						<TableHead>DETAILS</TableHead>
+					</TableRow>
+				</TableHeader>
+				<Suspense fallback={<Loader2Icon className="h-10 w-10 text-center py-6 text-blue-500 animate-spin" />} >
+					<BranchList />
+				</Suspense>
+			</Table>
+
+		</div>
 	)
 }
 
-export default Dashboard
+export default page
--- src/components/DeleteButton.tsx
+++ src/components/DeleteButton.tsx
@@ -15,27 +15,29 @@ import {
 import { useMutation } from '@tanstack/react-query';
 import axios from 'axios';
 import toast from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
 
 
 function DeleteButton({ username }: { username: string }) {
-	// const { mutate, isPending } = useMutation({
-	// 	mutationFn: async (username: string) => {
-	// 		const response = await axios.delete(`/api/branch/${username}`);
-	// 		return response.data;
-	// 	},
-	// });
+	const router = useRouter();
+	const { mutate, isPending } = useMutation({
+		mutationFn: async (username: string) => {
+			const response = await axios.delete(`/api/branch/${username}`);
+			return response.data;
+		},
+	});
 	// // Deleted handler
 	function handleDeleted(username: string) {
-		// mutate(username, {
-		// 	onSuccess: (data: any) => {
-		// 		// console.log(data);
-		// 		toast.success("Products Deleted Successfully");
-		// 	},
-		// 	onError: (error) => {
-		// 		toast.error("Products Delete Failed");
-		// 	}
-		// });
-		console.log({ username })
+		mutate(username, {
+			onSuccess: (data: any) => {
+				// console.log(data);
+				router.refresh();
+				toast.success("Products Deleted Successfully");
+			},
+			onError: (error) => {
+				toast.error("Products Delete Failed");
+			}
+		});
 
 	}
 	return (
@@ -48,10 +50,10 @@ function DeleteButton({ username }: { username: string }) {
 					<AlertDialogHeader>
 						<AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
 					</AlertDialogHeader>
-					<AlertDialogFooter>
+					{isPending ? <Button disabled>Loading...</Button> : <AlertDialogFooter>
 						<AlertDialogCancel className=' bg-color-main hover:bg-color-main'>Cancel</AlertDialogCancel>
 						<AlertDialogAction className=' bg-color-sub hover:bg-color-sub' onClick={() => handleDeleted(username)}>Continue</AlertDialogAction>
-					</AlertDialogFooter>
+					</AlertDialogFooter>}
 				</AlertDialogContent>
 			</AlertDialog>
 
