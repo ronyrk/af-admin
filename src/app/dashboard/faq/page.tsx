a59e1cd727d67206cc9685c9c4d4f611927e8cb9
rakibulhasan933 <rakibulmd933@gmail.com>
2024-02-26 23:58:30 +0600

    [ADD] solved

--- src/app/dashboard/faq/page.tsx
+++ src/app/dashboard/faq/page.tsx
@@ -1,12 +1,15 @@
-import CreateFAQ from "@/components/CreateFAQ";
+import CreateFAQ from '@/components/CreateFAQ'
+import React from 'react'
 
-
-export default function FAQ() {
-
+const page = () => {
 	return (
-		<div className=" flex flex-col gap-2">
-			<h2 className=" text-xl text-color-main text-center">FAQ</h2>
-			<CreateFAQ />
+		<div>
+			<div className=" flex flex-col gap-2">
+				<h2 className=" text-xl text-color-main text-center">FAQ</h2>
+				<CreateFAQ />
+			</div>
 		</div>
 	)
 }
+
+export default page
