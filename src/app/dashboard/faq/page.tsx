56ace72d49b5266bc7626b558cf2bad7273508b5
rakibulhasan933 <rakibulmd933@gmail.com>
2024-02-26 23:55:08 +0600

    [ADD] text area

---
+++ src/app/dashboard/faq/page.tsx
@@ -0,0 +1,12 @@
+import CreateFAQ from "@/components/CreateFAQ";
+
+
+export default function FAQ() {
+
+	return (
+		<div className=" flex flex-col gap-2">
+			<h2 className=" text-xl text-color-main text-center">FAQ</h2>
+			<CreateFAQ />
+		</div>
+	)
+}
---
+++ src/components/Toolbar.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { type Editor } from "@tiptap/react"
+import { Bold, Strikethrough, Italic, List, ListOrdered, Heading2 } from "lucide-react";
+import { Toggle } from "@/components/ui/toggle"
+
+type Props = {
+	editor: Editor | null
+};
+
+function Toolbar({ editor }: Props) {
+	if (!editor) {
+		return null
+	};
+
+	return (
+		<div className='border border-input bg-transparent rounded-md p-1'>
+			<Toggle size="sm"
+				pressed={editor.isActive("heading")}
+				onPressedChange={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+			>
+				<Heading2 className='w-4 h-4' />
+			</Toggle>
+			<Toggle size="sm"
+				pressed={editor.isActive("bold")}
+				onPressedChange={() => editor.chain().focus().toggleBold().run()}
+			>
+				<Bold className='w-4 h-4' />
+			</Toggle>
+			<Toggle size="sm"
+				pressed={editor.isActive("italic")}
+				onPressedChange={() => editor.chain().focus().toggleItalic().run()}
+			>
+				<Italic className='w-4 h-4' />
+			</Toggle>
+			<Toggle size="sm"
+				pressed={editor.isActive("strike")}
+				onPressedChange={() => editor.chain().focus().toggleStrike().run()}
+			>
+				<Strikethrough className='w-4 h-4' />
+			</Toggle>
+			<Toggle size="sm"
+				pressed={editor.isActive("bulletList")}
+				onPressedChange={() => editor.chain().focus().toggleBulletList().run()}
+			>
+				<List className='w-4 h-4' />
+			</Toggle>
+			<Toggle size="sm"
+				pressed={editor.isActive("orderedList")}
+				onPressedChange={() => editor.chain().focus().toggleBulletList().run()}
+			>
+				<ListOrdered className='w-4 h-4' />
+			</Toggle>
+		</div>
+	)
+}
+
+export default Toolbar
