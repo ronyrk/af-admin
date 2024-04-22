f33f9b60454c32a23e51fda3699215ecfa4b5126
rakibulhasan933 <rakibulmd933@gmail.com>
2024-04-22 23:45:06 +0600

    [ADD] deleted and updated

--- src/app/dashboard/borrowers/[username]/page.tsx
+++ src/app/dashboard/borrowers/[username]/page.tsx
@@ -1,12 +1,23 @@
+import BorrowerUpdated from '@/components/BorrowersUpdated';
+import { LoanIProps } from '@/types';
+import { unstable_noStore } from 'next/cache';
 import React from 'react'
 
-function page({ params }: {
+async function page({ params }: {
 	params: {
 		username: string;
 	}
 }) {
+	unstable_noStore();
+	let res = await fetch(`https://arafatfoundation.vercel.app/api/loan/${params.username}`);
+	if (!res.ok) {
+		throw new Error("Failed to fetch data list");
+	};
+	const borrowers: LoanIProps = await res.json();
 	return (
-		<div>page-{params.username}</div>
+		<div>
+			<BorrowerUpdated data={borrowers} />
+		</div>
 	)
 }
 
