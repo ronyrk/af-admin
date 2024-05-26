284dd3040c3c84760e8ed356323b66690ca91055
rakibulhasan933 <rakibulmd933@gmail.com>
2024-05-26 15:34:48 +0600

    [ADD]Expenses

---
+++ src/app/api/expenses/route.ts
@@ -0,0 +1,33 @@
+import prisma from "@/lib/prisma"
+import { ExpensesIProps } from "@/types";
+import { NextResponse } from "next/server";
+
+
+export const dynamic = 'force-dynamic'
+
+export const GET = async () => {
+	try {
+		const result = await prisma.expenses.findMany();
+		return NextResponse.json(result);
+	} catch (error) {
+		throw new Error("Server Error");
+	}
+};
+
+export const POST = async (request: Request) => {
+	try {
+		const body: ExpensesIProps = await request.json();
+		const { amount, description, date } = body;
+		const result = await prisma.expenses.create({
+			data: {
+				amount, description, date
+			}
+		});
+		return NextResponse.json({ message: "Successfully Branch Created", result }, { status: 201 });
+	} catch (error: any) {
+		if (error?.code === 'P2002') {
+			return NextResponse.json({ message: `a new user cannot be created with this ${error?.meta?.target}` });
+		}
+		return NextResponse.json({ message: "Expenses Created Failed" });
+	}
+}
