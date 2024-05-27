a8911145abbc1b4d13094f1235272df88718667b
rakibulhasan933 <rakibulmd933@gmail.com>
2024-05-27 10:05:06 +0600

    [ADD] income updated

--- src/components/IncomeUpdated.tsx
+++ src/components/IncomeUpdated.tsx
@@ -13,74 +13,65 @@ import {
 	FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-import { useMutation, useQuery } from "@tanstack/react-query"
+import { useMutation } from "@tanstack/react-query"
 import axios from "axios"
 import toast from "react-hot-toast"
 import { useRouter } from "next/navigation"
-import { ChildIProps, DisbursementIProps } from "@/types"
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
+import { IncomeIProps } from "@/types"
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
 import { CalendarIcon } from "lucide-react"
 import { Calendar } from "@/components/ui/calendar"
 import { cn } from "@/lib/utils"
 import { format } from "date-fns"
-import TailwindEditor from "@/components/editor"
 
 
 const formSchema = z.object({
-	username: z.string(),
-	description: z.string(),
+	type: z.string(),
+	transaction: z.string(),
 	amount: z.string(),
 	date: z.date({
 		required_error: "A date is required.",
 	}),
 });
 
-
-function DonorPaymentCreate() {
+function IncomeUpdated({ data }: { data: IncomeIProps }) {
 	const router = useRouter();
 
 	// 1. Define your form.
 	const form = useForm<z.infer<typeof formSchema>>({
 		resolver: zodResolver(formSchema),
-
+		defaultValues: {
+			date: data.date,
+			amount: data.amount,
+			transaction: data.transaction,
+			type: data.type
+		}
 	});
 
 	const { mutate, isPending } = useMutation({
-		mutationFn: async ({ username, date, amount, description }: DisbursementIProps) => {
-			const response = await axios.post("/api/disbursement", {
-				username, date, amount, description
+		mutationFn: async ({ date, amount, type, transaction }: IncomeIProps) => {
+			const response = await axios.patch(`/api/income/${data.id}`, {
+				date, amount, type, transaction
 			});
 			return response.data;
 		},
 	});
-	// Branch List
-	const { data, isLoading } = useQuery<ChildIProps[]>({
-		queryKey: ["child"],
-		queryFn: async () => {
-			const response = await axios.get('/api/child');
-			return response.data;
-		},
-		refetchInterval: 10000,
-	});
+
 
 	// 2. Define a submit handler.
 	function onSubmit(values: z.infer<typeof formSchema>) {
 		const previous = values.date;
 		const date = new Date(previous);
 		date.setDate(previous.getDate() + 1);
-		const username = values.username;
-		const description = values.description;
+		const transaction = values.transaction;
+		const type = values.type;
 		const amount = values.amount;
-		// Donor /Lender Payment Created
-		mutate({ username, date, description, amount }, {
-			onSuccess: (data: DisbursementIProps) => {
+
+		mutate({ date, amount, type, transaction }, {
+			onSuccess: ({ message, data }: { message: string, data: IncomeIProps }) => {
 				if (data?.id) {
-					toast.success("Disbursement Create Successfully");
-				} else {
-					throw new Error("Disbursement Created Failed")
+					toast.success(message);
 				}
-				router.push(`/dashboard/disbursement`);
 				router.refresh();
 			},
 			onError: (error) => {
@@ -88,48 +79,17 @@ function DonorPaymentCreate() {
 			}
 		});
 	};
-	// console.log(state, stateBranch);
-
 	return (
-		<div className="flex flex-col gap-3">
-			<h2 className="text-center text-xl">Donor Payment Create</h2>
+		<div className="flex flex-col  gap-3">
+			<h2 className="text-center text-xl">Income Create</h2>
 			<Form {...form}>
 				<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
-					<div className=" grid grid-cols-1 items-center gap-3">
-
-						<FormField
-							control={form.control}
-							name="username"
-							render={({ field }) => (
-								<FormItem>
-									<FormLabel>Child</FormLabel>
-									<FormControl>
-										<Select onValueChange={field.onChange} defaultValue={field.value}>
-											<FormControl>
-												<SelectTrigger>
-													<SelectValue placeholder="Select a verified Child" />
-												</SelectTrigger>
-											</FormControl>
-											<SelectContent>
-												{
-													data?.map((item, index) => (
-
-														<SelectItem key={index} value={item.username}>{item.name}</SelectItem>
-
-													))
-												}
-											</SelectContent>
-										</Select>
-									</FormControl>
-									<FormMessage />
-								</FormItem>
-							)}
-						/>
+					<div className=" grid grid-cols-2 justify-items-stretch gap-3">
 						<FormField
 							control={form.control}
 							name="date"
 							render={({ field }) => (
-								<FormItem className="flex flex-col">
+								<FormItem className="flex flex-col mt-[10px]">
 									<FormLabel>Date</FormLabel>
 									<Popover>
 										<PopoverTrigger asChild>
@@ -173,7 +133,7 @@ function DonorPaymentCreate() {
 								<FormItem>
 									<FormLabel>Amount</FormLabel>
 									<FormControl>
-										<Input type="number" placeholder="amount" {...field} />
+										<Input defaultValue={data.amount} type="number" placeholder="amount" {...field} />
 									</FormControl>
 									<FormMessage />
 								</FormItem>
@@ -181,24 +141,35 @@ function DonorPaymentCreate() {
 						/>
 						<FormField
 							control={form.control}
-							name="description"
+							name="type"
 							render={({ field }) => (
-								<FormItem className="">
-									<FormLabel>Description</FormLabel>
-									<FormControl className="">
-										<TailwindEditor description={field.name} onChange={field.onChange} value={field.value} />
+								<FormItem>
+									<FormLabel>Payment method</FormLabel>
+									<FormControl>
+										<Input defaultValue={data.type} placeholder="method" {...field} />
 									</FormControl>
 									<FormMessage />
 								</FormItem>
 							)}
 						/>
-
+						<FormField
+							control={form.control}
+							name="transaction"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Transaction ID</FormLabel>
+									<FormControl>
+										<Input defaultValue={data.transaction} placeholder="Transaction ID" {...field} />
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
 					</div>
 					{isPending ? <Button disabled >Loading...</Button> : <Button type="submit">Submit</Button>}
 				</form>
-			</Form>
-		</div>
+			</Form></div>
 	)
 }
 
-export default DonorPaymentCreate;
+export default IncomeUpdated
