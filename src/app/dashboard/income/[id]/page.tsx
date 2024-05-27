77c0e8b5af3415b927346bbff2d7b5732a7bdc74
rakibulhasan933 <rakibulmd933@gmail.com>
2024-05-27 10:31:11 +0600

    [ADD] income

--- src/app/dashboard/income/[id]/page.tsx
+++ src/app/dashboard/income/[id]/page.tsx
@@ -1,7 +1,7 @@
 import IncomeUpdated from "@/components/IncomeUpdated";
 import { IncomeIProps } from "@/types";
 import { unstable_noStore } from "next/cache";
-
+import prisma from "@/lib/prisma";
 
 
 async function Income({ params }: {
@@ -9,16 +9,17 @@ async function Income({ params }: {
 		id: string
 	}
 }) {
+	const id = params.id;
 	unstable_noStore();
-	const res = await fetch(`https://af-admin.vercel.app/api/income/${params.id}`);
-	if (!res.ok) {
-		throw new Error("Failed to fetch data");
-	};
-	const data: IncomeIProps = await res.json();
+	const data = await prisma.income.findUnique({
+		where: {
+			id,
+		}
+	});
 
 	return (
 		<div>
-			<IncomeUpdated data={data} />
+			<IncomeUpdated data={data as IncomeIProps} />
 		</div>
 	)
 }
