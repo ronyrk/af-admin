755e115880efc5cd7f625e487b18ffd3e514d765
rakibulhasan933 <rakibulmd933@gmail.com>
2024-07-17 11:51:00 -0700

    [ADD] searc

--- src/app/api/income-search/route.ts
+++ src/app/api/income-search/route.ts
@@ -1,12 +1,44 @@
 import { NextResponse } from "next/server";
-
+import prisma from "@/lib/prisma";
 
 export const GET = async (request: Request) => {
     const url = new URL(request.url);
-    const start = url.searchParams.get("from");
-    const end = url.searchParams.get("to");
+    const startString = url.searchParams.get("from");
+    const start = new Date(startString as any);
+    const endString = url.searchParams.get("to");
+    const end = new Date(endString as any)
     const page = url.searchParams.get("page");
-    const transaction = url.searchParams.get("transaction");
-    console.log(request.url);
-    return NextResponse.json("ok");
+    const transaction = url.searchParams.get("transaction") as any;
+    console.log(startString, endString, page, transaction);
+    try {
+        if (transaction === "") {
+            const result = await prisma.income.findMany({
+                // 	// skip,
+                // 	// take,
+                where: {
+                    date: {
+                        gte: start,
+                        lte: end
+                    }
+                },
+                orderBy: {
+                    date: "desc"
+                }
+            });
+            return NextResponse.json(result)
+        } else {
+            const result = await prisma.income.findMany({
+                where: {
+                    transaction: {
+                        contains: transaction,
+                        mode: "insensitive"
+                    }
+                }
+            });
+            return NextResponse.json(result);
+        }
+    } catch (error) {
+        return NextResponse.json("server Error");
+    }
+
 }
