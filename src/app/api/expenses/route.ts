f70cb0caae5ac0ba06148a4eeb20071963fa8b3b
rakibulhasan933 <rakibulmd933@gmail.com>
2024-07-18 06:45:10 -0700

    [ADD] expenses create

--- src/app/api/expenses/route.ts
+++ src/app/api/expenses/route.ts
@@ -23,7 +23,7 @@ export const POST = async (request: Request) => {
 				amount, description, date
 			}
 		});
-		return NextResponse.json({ message: "Successfully Branch Created", result }, { status: 201 });
+		return NextResponse.json({ message: "Successfully expenses Created", result }, { status: 201 });
 	} catch (error: any) {
 		if (error?.code === 'P2002') {
 			return NextResponse.json({ message: `a new user cannot be created with this ${error?.meta?.target}` });
