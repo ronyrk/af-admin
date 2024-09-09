f11aa721140ddbd46c6d204abeceb84ff2800c28
MD RAKIBUL HASAN <76769537+rakibulhasan933@users.noreply.github.com>
2024-09-09 23:53:28 +0600

    Update route.ts

--- src/app/api/donation/[username]/route.ts
+++ src/app/api/donation/[username]/route.ts
@@ -8,7 +8,7 @@ export const dynamic = 'force-dynamic'
 export const GET = async (request: Request, { params }: ParamsIProps) => {
 	try {
 		const { username } = params;
-		const result = await prisma.ChilsDonate.findMany({
+		const result = await prisma.ChildsDonate.findMany({
 			where: {
 				ChildName:username,
 			}
